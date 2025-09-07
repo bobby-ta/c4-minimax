8bcad55890139dbed6bf28571780b360017a377e
Bobby Ta <49725144+bobby-ta@users.noreply.github.com>
2025-09-07 20:18:13 +0200

    Add heuristic function

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ class Game {
     private:
     std::array<std::array<int, COLS>, ROWS> board;
     int player = 1; //player = 1, bot = -1
+    std::array<int, 2> lastMove;
     struct moveResult {
         bool moveValid;
         int row; //0-ROWS if successful, -1 if unsuccessful
@@ -53,12 +54,13 @@ class Game {
     std::string makeMove(int col) {
         this->result.col = col;
         int row = this->whereTopRow(col);
+        this->lastMove = {row, col};
 
         if (row != -1) {
             this->result.moveValid = true;
             this->result.row = row;
             this->board[row][col] = this->player; //Put move down
-            this->result.win = this->checkWin(row, col);
+            this->result.win = this->checkWin();
             this->result.boardFull = this->boardFull();
 
             this->player *= -1; //Switch player only once board state has been altered;
@@ -79,7 +81,9 @@ class Game {
         return -1;
     }
 
-    bool checkWin(int row, int col) {
+    bool checkWin() {
+        int row = this->lastMove[0];
+        int col = this->lastMove[1];
         const int vectors[4][2] {
             {0, 1}, //hor
             {1, 0}, //vert
@@ -116,6 +120,33 @@ class Game {
             }
         }
         return true;
+    }
+
+    int evalBoardState() {
+        int squareScores[6][7] = {
+            {3, 4, 5, 6, 5, 4, 3},
+            {4, 6, 8, 10, 8, 6, 4},
+            {5, 8, 11, 13, 11, 8, 5},
+            {5, 8, 11, 13, 11, 8, 5},
+            {4, 6, 8, 10, 8, 6, 4},
+            {3, 4, 5, 6, 5, 4, 3}
+        };
+        int score = 0;
+        for (int c=0;c<COLS;c++) {
+            for (int r=0;r<ROWS;r++) {
+                if (this->board[r][c] == 0) {
+                    break; //Don't waste time multiplying the 0s
+                } else {
+                    score += (this->board[r][c]) * (squareScores[r][c]);
+                }
+            }
+        }
+
+        //Remember the bot is marked -1: this means bot advantage yields low score
+        //But we always want to maximise for the bot and minimise for the player
+        //Therefore we switch signs to make make eval more intuitive - high scores for bot advantage
+        //So that we don't get muddled up with min and max and minuses in the minimax function itself
+        return -score;
     }
 };
 
